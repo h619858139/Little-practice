abe942855cc6312139b3714ca15c6e9200b2431c
HZD <619858139@qq.com>
2021-03-02 20:35:07 +0800

    init commit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+      <p> 今天不是一个人 </p>
+	<p>啦啦啦</p>
+   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
